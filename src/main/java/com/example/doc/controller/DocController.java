8f9bd9262e52cb5ee9a825085e16d210e1293502
Leo <296934109@qq.com>
2021-08-19 08:26:40 +0800

    第一次修改

--- src/main/java/com/example/doc/controller/DocController.java
+++ src/main/java/com/example/doc/controller/DocController.java
@@ -1,9 +1,16 @@
 package com.example.doc.controller;
 
+import com.example.doc.entity.Doc;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/doc")
 public class DocController {
+
+    public List<Doc> selectDoc(){
+        return null;
+    }
 }
---
+++ src/main/java/com/example/doc/entity/Req.java
@@ -0,0 +1,5 @@
+package com.example.doc.entity;
+
+public class Req {
+
+}
